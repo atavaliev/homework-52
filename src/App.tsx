b09abe2990472381ebef0f714a28e5d466be82a8
Atavaliev Timur <tatavaliev@gmail.com>
2023-05-19 14:25:28 +0600

    Step 3: Show(Get) cards by Clicking button added check for the number of remaining cards

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,72 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import CardView from "./CardView/CardView";
 import CardDeck from "./lib/CardDeck";
+import Card from "./lib/Card";
 
 
 const App = () => {
 
-    const deck = new CardDeck();
-    console.log(deck.getCards(5))
+    const [cards, setCards] = useState<CardDeck>(new CardDeck());
+    const [handCards, setHandCards] = useState<Card[]>([]);
+
+    const getCardsToHand = (howmany: number = 5) => {
+        setHandCards(cards.getCards(howmany))
+    }
+
+    //Reset Cards and Deck to default values
+    const setNewCards = () => {
+        setHandCards([]);
+        setCards(new CardDeck());
+    }
 
 
     return (
         <div className="App">
-             <CardView rank="5" suit="spades"/>
+
+            <>
+                {
+                    //Check is there any Cards on the Table
+                    handCards.length < 1
+                        ? <>
+                            <p>There is no Cards</p>
+                            <button onClick={() => getCardsToHand()}>Get Cards</button>
+                        </>
+                        //Check if there are less 5 cards so get only available quantity of cards
+                        : cards.deck.length < 5
+                            ? <>
+                                <div className="handCards">
+                                    {
+                                        handCards.map(c => {
+                                            return <CardView key={c.rank + c.suit} rank={c.rank} suit={c.suit}/>
+                                        })
+                                    }
+                                </div>
+
+                                {cards.deck.length > 0 && <button onClick={() => getCardsToHand(cards.deck.length)}>Show
+                                    Last {cards.deck.length} Cards
+                                </button>}
+
+                                {cards.deck.length <= 0 && <>
+                                    <p>There is no Cards in Desk</p>
+                                    <button onClick={() => setNewCards()}>New Game</button>
+                                </>
+
+                                }
+                            </>
+                            //Show new 5 cards from the deck every time when you click button
+                            : <>
+                                <div className="handCards">
+                                    {
+                                        handCards.map(c => {
+                                            return <CardView key={c.rank + c.suit} rank={c.rank} suit={c.suit}/>
+                                        })
+                                    }
+                                </div>
+                                <button onClick={() => getCardsToHand()}>Get Another 5 Cards</button>
+                            </>
+                }
+            </>
         </div>
     )
 };
