dcf6254839329bf613d7555dec1c20954915a3c9
Atavaliev Timur <tatavaliev@gmail.com>
2023-05-19 11:07:22 +0600

    Step 1: Create Component CardView with 2 props(rank, suit)

---
+++ src/CardView/CardView.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+interface ICardView {
+    rank: string;
+    suit: string
+}
+
+const CardView: React.FC<ICardView> = ({rank, suit}) => {
+
+        const cardClasses = `card rank-${rank} ${suit}`
+
+        return (
+            <div className="playingCards faceImages">
+            <span className={cardClasses.toLowerCase()}>
+                <span className="rank">{rank}</span>
+                <span className="suit">
+                    {
+                        suit === "diams"
+                            ? "♦" : "hearts "
+                                ? "♥" : "clubs"
+                                    ? "♣" : "♠"
+                    }
+                </span>
+            </span>
+            </div>
+        );
+    }
+;
+
+export default CardView;
